55c2dab9537a9832199210e822213a69a61e2889
Artur Zieliński <artur.zielinski87@gmail.com>
2026-01-18 16:47:37 +0100

    add Personal_card class

---
+++ main.py
@@ -0,0 +1,9 @@
+#Personal cards database
+
+class Personal_card:
+    def __init__(self, name, surname, company, job, email):
+        self.name = name
+        self.surname = surname
+        self.company = company
+        self.occupation = job
+        self.email = email
