908d012fffbdc95c4a657b4c31e87ec866fbac95
Artur Zieliński <artur.zielinski87@gmail.com>
2026-01-18 16:56:33 +0100

    add names data

--- main.py
+++ main.py
@@ -16,3 +16,11 @@ class Personal_card:
     @property
     def name_length(self):
         return f'Name length is: {len(self.name + self.surname) + 1}'
+
+#Names database
+
+person1 = Person(name="Elizabeth", surname="Martinez", company="Muscle Factory", job="Oceanographer", email="ElizabethMMartinez@dayrep.com")
+person2 = Person(name="Carl", surname="McCarthy", company="Foxmoor", job="Labor relations manager", email="CarlDMcCarthy@jourrapide.com")
+person3 = Person(name="Kimberly", surname="Tyner", company="Zany Brainy", job="Nuclear medicine technologist", email="KimberlyTTyner@dayrep.com")
+person4 = Person(name="Buford", surname="Duncan", company="Paul's Record Hut", job="Forester", email="BufordCDuncan@armyspy.com")
+person5 = Person(name="Michelle", surname="McCormick", company="KG Menswear", job="Municipal court judge", email="MichelleBMcCormick@teleworm.us")
